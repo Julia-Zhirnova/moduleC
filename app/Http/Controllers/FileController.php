19b2d92ea53d7d2e23c4acebf8d76f2d73afe9fd
rexam <rexam@localhost.localdomain>
2024-08-06 03:36:28 +0300

    17 commit

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -18,10 +16,10 @@ class FileController extends Controller
 
         if ($validator->fails()) {
             return response()->json([
-                'success' => false,
-                'code' => 422,
-                'message' => $validator->errors(),
-            ]);
+            'success' => false,
+            'code' => 422,
+            'message' => $validator->errors()
+            ]);                
         }
 
         $file = $request->file('files');
